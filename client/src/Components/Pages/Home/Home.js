c5af218f40db359ae6ca13adc8a7f165496e6cc2
marasinipradeep <marasinipradeep@gmail.com>
2020-10-01 11:45:32 +0930

    Working on Hero image

--- client/src/Components/Pages/Home/Home.js
+++ client/src/Components/Pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Header from "../../Header/Header"
+import Hero from "../../Hero/Hero"
+import Banner from "../../Banner/Banner"
 
 export default function Home() {
     return (
         <div>
-            <Header/>
+            <Header />
+            <Hero>
+                <Banner title="Connect to employees" subtitle="Hire Trusted And Skilled Employee">
+                    <Link to="/employee" className="btn-primary">
+                        Find Employee
+        </Link>
+                </Banner>
+            </Hero>
+
         </div>
     )
 }
