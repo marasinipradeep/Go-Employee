9e5fdc304ef1583e5bb385ecaca0ba675dbc383f
marasinipradeep <marasinipradeep@gmail.com>
2020-10-01 16:45:45 +0930

    Working on employeee dashboard material theme

--- client/src/Components/Pages/EmployeePortal/EmployeeDashboard.js
+++ client/src/Components/Pages/EmployeePortal/EmployeeDashboard.js
@@ -1,5 +1,6 @@
-import React,{useContext} from 'react'
-import {useHistory} from "react-router-dom"
+import React, { useContext } from 'react'
+import { useHistory } from "react-router-dom";
+import Adminheader from "../../AdminHeader"
 
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -8,165 +9,166 @@ import TextField from '@material-ui/core/TextField';
 import UserContext from "../../../context/UserContext"
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: '25ch',
-  },
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: '25ch',
+    },
 }));
 
 
 
-const  EmployeeDashboard=()=> {
+const EmployeeDashboard = () => {
     const classes = useStyles();
 
-     //When we useContext we get setvalue in so destructure the data from the UserContext which is provideded from provider
-     const { userData, setUserData } = useContext(UserContext)
+    //When we useContext we get setvalue in so destructure the data from the UserContext which is provideded from provider
+    const { userData, setUserData } = useContext(UserContext)
 
-     //useHistory gives result in an array
+    //useHistory gives result in an array
     const history = useHistory();
 
     const logout = () => {
         setUserData({
-            token:undefined,
-            employee:undefined
+            token: undefined,
+            employee: undefined
 
         });
-        localStorage.setItem("auth-token","")
+        localStorage.setItem("auth-token", "")
         history.push("/")
-      
+
     }
     console.log("inside employee dashboard")
     return (
         <div>
-       <div>I am employee dashboard</div>
-       <button onClick={logout}>Logout</button>
-       <div className={classes.root}>
-      <div>
-        <TextField
-          id="standard-full-width"
-          label="Label"
-          style={{ margin: 8 }}
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          label="None"
-          id="margin-none"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-        />
-        <TextField
-          label="Dense"
-          id="margin-dense"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          margin="dense"
-        />
-        <TextField
-          label="Normal"
-          id="margin-normal"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          margin="normal"
-        />
-      </div>
-      <div>
-        <TextField
-          id="filled-full-width"
-          label="Label"
-          style={{ margin: 8 }}
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          label="None"
-          id="filled-margin-none"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          variant="filled"
-        />
-        <TextField
-          label="Dense"
-          id="filled-margin-dense"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          margin="dense"
-          variant="filled"
-        />
-        <TextField
-          label="Normal"
-          id="filled-margin-normal"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          margin="normal"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          id="outlined-full-width"
-          label="Label"
-          style={{ margin: 8 }}
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-        <TextField
-          label="None"
-          id="outlined-margin-none"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          variant="outlined"
-        />
-        <TextField
-          label="Dense"
-          id="outlined-margin-dense"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          margin="dense"
-          variant="outlined"
-        />
-        <TextField
-          label="Normal"
-          id="outlined-margin-normal"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          margin="normal"
-          variant="outlined"
-        />
-      </div>
-    </div>
-       </div>
+            <Adminheader/>
+            
+            <button onClick={logout}>Logout</button>
+            <div className={classes.root}>
+                <div>
+                    <TextField
+                        id="standard-full-width"
+                        label="Label"
+                        style={{ margin: 8 }}
+                        placeholder="Placeholder"
+                        helperText="Full width!"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <TextField
+                        label="None"
+                        id="margin-none"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                    />
+                    <TextField
+                        label="Dense"
+                        id="margin-dense"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                        margin="dense"
+                    />
+                    <TextField
+                        label="Normal"
+                        id="margin-normal"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                        margin="normal"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="filled-full-width"
+                        label="Label"
+                        style={{ margin: 8 }}
+                        placeholder="Placeholder"
+                        helperText="Full width!"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        variant="filled"
+                    />
+                    <TextField
+                        label="None"
+                        id="filled-margin-none"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                        variant="filled"
+                    />
+                    <TextField
+                        label="Dense"
+                        id="filled-margin-dense"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                        margin="dense"
+                        variant="filled"
+                    />
+                    <TextField
+                        label="Normal"
+                        id="filled-margin-normal"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                        margin="normal"
+                        variant="filled"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="outlined-full-width"
+                        label="Label"
+                        style={{ margin: 8 }}
+                        placeholder="Placeholder"
+                        helperText="Full width!"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        variant="outlined"
+                    />
+                    <TextField
+                        label="None"
+                        id="outlined-margin-none"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                        variant="outlined"
+                    />
+                    <TextField
+                        label="Dense"
+                        id="outlined-margin-dense"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                        margin="dense"
+                        variant="outlined"
+                    />
+                    <TextField
+                        label="Normal"
+                        id="outlined-margin-normal"
+                        defaultValue="Default Value"
+                        className={classes.textField}
+                        helperText="Some important text"
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
