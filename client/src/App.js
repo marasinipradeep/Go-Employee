0c278de53a009c3470f111ebffcc6a1e1d43d115
marasinipradeep <marasinipradeep@gmail.com>
2020-10-01 14:57:48 +0930

    Setting authentication token in front end and restricting routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React, {useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Axios from "axios";
 
 
 //Importing Styles
@@ -11,6 +12,7 @@ import "./style.css"
 import Header from "./Components/Header/Header";
 import Login from "./Components/auth/Login";
 import Register from "./Components/auth/Register";
+import EmployeeDashboard from "./Components/Pages/EmployeePortal/EmployeeDashboard"
 
 //Importing pages
 import Home from "./Components/Pages/Home/Home"
@@ -26,16 +28,44 @@ function App() {
     token: undefined,
     user: undefined
   });
+
+  //useEffect requires function as parameters,has dependency list[] which is an array,when state changes rerender the Effect
+  //If array is empty just going to run once.
+  //So this basically function that runs when app starts.Downside cant have asynchrouns function.
+  useEffect(() => {
+    console.log("inside use effect")
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem("auth-token");//When empty Null or undefined
+      if(token ===null){
+        localStorage.setItem("auth-token","");
+        token ="";
+      }
+      const tokenRes = await Axios.post("http://localhost:8080/employee/tokenIsValid", null, 
+      { headers: { "x-auth-token": token }});
+     if(tokenRes.data){
+       const employeeRes=await Axios.get("http://localhost:8080/employee",
+       {headers:{"x-auth-token":token}
+     });
+
+     setUserData({
+       token,
+       employee:employeeRes.data,
+     });
+
+     }
+    };
+    checkLoggedIn();
+  }, []);
   return (
     <Router>
       <UserContext.Provider value={{ userData, setUserData }}>
-     
         <Header />
         <div>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/login/employee/dashboard" component={EmployeeDashboard} />
           </Switch>
         </div>
       </UserContext.Provider>
---
+++ client/src/Components/Pages/EmployeePortal/EmployeeDashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const  EmployeeDashboard=()=> {
+    console.log("inside employee dashboard")
+    return (
+       <div>I am employee dashboard</div>
+    )
+}
+
+export default EmployeeDashboard;
